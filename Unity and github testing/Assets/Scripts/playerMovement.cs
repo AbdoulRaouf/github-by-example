9b289614646a57dec8091f2120f53c0d32584b07
APPI72020 <abdoulraouf72020@gmail.com>
2022-07-20 22:35:30 +0100

    2em modification

---
+++ Unity and github testing/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("EZE");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
